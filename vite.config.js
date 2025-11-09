6ff011d49dcd379f88e910e69f22c0efca2360d3
takcr <ak24607@gmail.com>
2025-11-09 22:23:09 +0530

    some changes made to vite file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// IMPORTANT: base must be your repo name with leading/trailing slash
 export default defineConfig({
+  base: '/ai-stock-vault/',
   plugins: [react()],
 })
